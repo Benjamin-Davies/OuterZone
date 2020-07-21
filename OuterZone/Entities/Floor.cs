e0cda16b7337f492c154ec18e949f5b6e38edb1f
Benjamin Davies <benjamindavies@mmc.school.nz>
2020-07-21 15:09:48 +1200

    Add random terrain generation

--- OuterZone/Entities/Floor.cs
+++ OuterZone/Entities/Floor.cs
@@ -1,4 +1,5 @@
 ﻿using OuterZone.Entities.Base;
+using System;
 
 namespace OuterZone.Entities
 {
@@ -6,24 +7,42 @@ namespace OuterZone.Entities
     {
         private const int MaxListCount = 10;
         private const int DiscardCount = 5;
+        private const double InitialProbability = 0.1;
+        private const double FirstGap = 2;
 
         private double NextPosition = -20;
+        private double TileProbability = InitialProbability;
+        private Random Random = new Random();
 
         public void Generate(double rightEdge)
         {
             while (NextPosition < rightEdge)
             {
-                Children.Add(new FloorSection
-                {
-                    Position = (NextPosition, Position.Y),
-                });
-                NextPosition += FloorSection.Width;
+                GenerateTile();
             }
 
             if (Children.Count > MaxListCount)
             {
                 Children.RemoveRange(0, DiscardCount);
             }
+        }
+
+        public void GenerateTile()
+        {
+            if (NextPosition > FirstGap && Random.NextDouble() < TileProbability)
+            {
+                TileProbability = InitialProbability;
+            }
+            else
+            {
+                TileProbability *= 2;
+
+                Children.Add(new FloorSection
+                {
+                    Position = (NextPosition, Position.Y),
+                });
+            }
+            NextPosition += FloorSection.Width;
         }
     }
 }
