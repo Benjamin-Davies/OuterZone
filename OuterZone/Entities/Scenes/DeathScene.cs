46f7d51dade455fe1c3631df309c34acf9952349
Benjamin Davies <benjamindavies@mmc.school.nz>
2020-07-28 14:40:48 +1200

    Add you died message

--- OuterZone/Entities/Scenes/DeathScene.cs
+++ OuterZone/Entities/Scenes/DeathScene.cs
@@ -1,9 +1,13 @@
 ﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
 
 namespace OuterZone.Entities.Scenes
 {
     class DeathScene : Scene
     {
+        private static readonly string Message = "You Died";
+
         private readonly Scene GameScene;
 
         public DeathScene(ISceneManager sceneManager, Scene gameScene) : base(sceneManager)
@@ -18,7 +22,21 @@ namespace OuterZone.Entities.Scenes
             var background = new SolidBrush(Color.FromArgb(127, Color.Crimson));
             g.FillRectangle(background, Rectangle);
 
+            var scale = (float)Size.Y / 12;
+            var oldMatrix = g.Transform;
+            var matrix = new Matrix();
+            matrix.Translate((float)(Size.X / 2), (float)(Size.Y / 2));
+            matrix.Scale(scale, scale);
+            g.Transform = matrix;
+
+
+            var titleFont = new Font(Font.FontFamily, 1);
+            var titleSize = g.MeasureString(Message, titleFont);
+            g.DrawString(Message, titleFont, Brushes.White, new PointF(titleSize.Width / -2, -2 - titleSize.Height));
+
             base.Draw(g);
+
+            g.Transform = oldMatrix;
         }
     }
 }
--- OuterZone/Entities/Scenes/Scene.cs
+++ OuterZone/Entities/Scenes/Scene.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -13,6 +14,7 @@ namespace OuterZone.Entities.Scenes
     {
         public readonly ISceneManager SceneManager;
         public override Vector Size => (Vector)SceneManager.ClientSize;
+        public Font Font => MainWindow.DefaultFont;
 
         public Scene(ISceneManager sceneManager)
         {
