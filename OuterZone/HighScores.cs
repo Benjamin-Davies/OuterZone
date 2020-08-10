05ad6a89707527dde692f161c429a137845ad00c
Benjamin-Davies <bentendavies@gmail.com>
2020-08-10 15:15:16 +1200

    Remove failing google code

--- OuterZone/HighScores.cs
+++ OuterZone/HighScores.cs
@@ -1,6 +1,4 @@
-﻿using Google.Cloud.Firestore;
-using Google.Cloud.Firestore.V1;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,24 +21,13 @@ namespace OuterZone
             }
         }
 
-        private readonly FirestoreDb db;
-        private readonly CollectionReference highScoresRef;
-
         private HighScores()
         {
-            var client = new FirestoreClientBuilder();
-            client.JsonCredentials = "";
-            db = FirestoreDb.Create(Properties.Resources.FirebaseProject, client.Build());
-            highScoresRef = db.Collection("high-scores");
         }
 
         async public Task<IEnumerable<HighScore>> GetHighScores()
         {
-            var highScores = await highScoresRef.OrderByDescending("score").Limit(10).GetSnapshotAsync();
-            return highScores.Select(s => new HighScore {
-                Name = s.GetValue<string>("name"),
-                Score = s.GetValue<int>("score"),
-            });
+            return null;
         }
 
         public class HighScore
